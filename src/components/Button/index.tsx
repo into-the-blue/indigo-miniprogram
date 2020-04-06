0b2d1f7622aaa4e905dd7eda83de192f9a98e8ca
Origami <q@timvel.com>
2020-04-06 20:48:39 +0800

    button styles

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,15 +4,22 @@ import { ButtonProps } from '@tarojs/components/types/Button';
 import classNames from 'classnames';
 import './index.scss';
 
-interface IProps extends ButtonProps {
+interface IProps extends Omit<ButtonProps, 'type'> {
   children: React.ReactChild;
+  type?: 'default' | 'primary' | 'danger';
 }
 const MButton = (props: IProps) => {
   let _className = props.className;
   if (typeof _className === 'string') {
     _className = classNames(_className);
   }
-  return <Button {...props} className={classNames('mbutton-default', _className)} />;
+  const _type = props.type || 'default';
+  const styles = classNames({
+    mbutton: true,
+    [`mbutton-${_type}`]: true,
+  });
+
+  return <Button {...props} type={undefined} className={classNames(styles, _className)} />;
 };
 
 export default MButton;
